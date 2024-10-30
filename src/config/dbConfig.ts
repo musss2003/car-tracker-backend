c19011fa669d02b5f8117a9199cc78d3a5f53cca
Mustafa Sinanovic <mustafasinanovic@outlook.com>
2024-10-30 15:32:46 +0100

    Added nodemon, fixed timeout on mongoose connection and added user role

--- src/config/dbConfig.ts
+++ src/config/dbConfig.ts
@@ -2,7 +2,12 @@ import mongoose from 'mongoose';
 
 export const connectToDb = async (): Promise<void> => {
     try {
-      await mongoose.connect(process.env.DB_URI || '');
+
+      await mongoose.connect(process.env.DB_URI || '', {
+        serverSelectionTimeoutMS: 5000, // Set the timeout for server selection
+        socketTimeoutMS: 45000, // Set the socket timeout
+    });
+    
       console.log('connected');
     } catch (e) {
       console.error('Error connecting to database:', e);
